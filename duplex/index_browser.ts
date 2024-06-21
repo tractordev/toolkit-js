f15deefb5b47eaee0bda95cfd672d138763853cd
Jeff Lindsay <progrium@gmail.com>
2024-06-21 15:07:17 -0400

    duplex: add messageport transport

--- duplex/index_browser.ts
+++ duplex/index_browser.ts
@@ -1,3 +1,4 @@
 export * from "./index.ts";
 export {Conn as FrameConn} from "./transport/iframe.ts";
 export {Conn as WorkerConn} from "./transport/worker.ts";
+export {Conn as PortConn} from "./transport/messageport.ts";
