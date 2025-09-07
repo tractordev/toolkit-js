049d7bb19b737c3f9a3ab88218e87a0e96a508af
Jeff Lindsay <progrium@gmail.com>
2025-09-06 21:25:24 -0700

    drop iframe and worker since we have messageport. drop index_browser.ts since we dont need it now

--- duplex/index.ts
+++ duplex/index.ts
@@ -1,8 +1,18 @@
+// @ts-ignore
 export * from "./codec/mod.ts";
+// @ts-ignore
 export * from "./rpc/mod.ts";
+// @ts-ignore
 export * from "./peer/mod.ts";
+// @ts-ignore
 export * from "./mux/mod.ts";
+// @ts-ignore
 export * from "./fn/mod.ts";
+// @ts-ignore
 export * from "./buffer.ts";
+// @ts-ignore
 export * from "./io.ts";
+// @ts-ignore
 export {Conn as PortConn} from "./transport/messageport.ts";
+// @ts-ignore
+export {Session as PortSession} from "./port/session.ts";
--- duplex/index_browser.ts
+++
@@ -1,3 +0,0 @@
-export * from "./index.ts";
-// export {Conn as FrameConn} from "./transport/iframe.ts";
-export {Conn as WorkerConn} from "./transport/worker.ts";
